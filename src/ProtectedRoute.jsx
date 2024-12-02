eddbdd7765ecf50d9393731d41f66695998b14e7
frosskiel <frosskiel@gmail.com>
2024-12-02 10:07:39 -0300

    fix: react/prop-types

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
   return token ? children : <Navigate to={"/auth/login"} />;
 }
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
